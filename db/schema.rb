0bb3e9f745e0d81daa4f5c3366ccd7fe035f904d
RemainZany <kredibleproperties@gmail.com>
2015-02-15 19:51:39 -0500

    rails_apps_composer: set up database

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150216005124) do
+ActiveRecord::Schema.define(version: 20150216005128) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "role"
   end
 
 end
